07284971b57e44bb763eb851ac70e8560e8d6730
keepcloudease <165644130@qq.com>
2025-09-22 22:54:54 +0800

    Site updated: 2025-09-22 22:54:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/20/hello-world/","2025/09/20/红点系统/"];function toRandomPost(){
+var posts=["2025/09/20/红点系统/","2025/09/22/事件系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
