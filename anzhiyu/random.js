4e668f7673176699529c10d15edc0810d3fb5919
keepcloudease <165644130@qq.com>
2025-10-04 01:52:28 +0800

    Site updated: 2025-10-04 01:52:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/20/红点系统/","2025/09/22/事件系统/","2025/09/23/新手引导/"];function toRandomPost(){
+var posts=["2025/09/20/红点系统/","2025/09/22/事件系统/","2025/09/23/新手引导/","2025/10/04/SLG大地图的管理方案/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
